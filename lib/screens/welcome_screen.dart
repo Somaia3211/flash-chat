41872c48e7dc120f549ad62c4541c40bebb30b5e
somaia3211 <somaiaahmadi@gmail.com>
2023-01-17 01:10:12 +0430

    fifth commit

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flash_chat_starting_project/constants.dart';
 import 'package:flash_chat_starting_project/screens/login_screen.dart';
 import 'package:flash_chat_starting_project/screens/registration_screen.dart';
@@ -5,6 +6,7 @@ import 'package:flash_chat_starting_project/screens/registration_screen.dart';
 import '../constants.dart';
 import '/constants.dart';
 import 'package:flutter/material.dart';
+import '';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_id';
@@ -19,27 +21,27 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   void initState() {
     super.initState();
-    controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1),
-        );
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
 
-    animation=ColorTween(
+    animation = ColorTween(
       begin: Colors.yellow.shade900,
-      end:kBackgroundColor,
+      end: kBackgroundColor,
     ).animate(controller);
     controller.forward();
     controller.addListener(() {
-      setState(() {
-
-      });
+      setState(() {});
     });
-
   }
-@override
+
+  @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,6 +60,18 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     height: 60,
                     child: Image.asset('images/logo.png'),
                   ),
+                ),
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.white,
+                  ),
+                  child: AnimatedTextKit(
+                    totalRepeatCount: 2,
+                      animatedTexts: [
+                    TypewriterAnimatedText('Flash Chat'),
+                  ]),
                 ),
                 Text(
                   'Flash Chat',
